e1cfac2777db4f17c120499cbc4ade9022a2c44d
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-07 02:58:01 +0600

    successfully done need to add css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// data calling function from api and format to json type
 const baseApi = 'https://www.themealdb.com/api/json/v1/1/search.php?f';
-
 const getFood = foodKeyWord => {
     const url = `${baseApi}=${foodKeyWord}`
     fetch(url)
@@ -7,25 +7,23 @@ const getFood = foodKeyWord => {
         .then(data => displayFood(data));
 }
 
+//clicking search button to call for data which go to getfood function
 document.getElementById('search').addEventListener('click', function () {
     const foodNames = document.getElementById('food-name').value;
     getFood(foodNames);
 });
 
+//display function for food name and photos which are similar to type letter
 const displayFood = food => {
-    // const fooName = food.strMeal[0];
-    // console.log(fooName);
-    // console.log(food.meals[0]);
     food.meals.forEach(singleFood => {
-        console.log(singleFood);
+        const mealArea = document.createElement('div');
+        const mealsDiv = document.getElementById('food-items');
+        mealArea.className = 'meal-area';
+        const mealInfo = `
+            <img class="meal-photo" src="${singleFood.strMealThumb}">
+            <h1 class="meal-name">${singleFood.strMeal}<h1>            
+        `
+        mealsDiv.appendChild(mealArea);
+        mealArea.innerHTML = mealInfo;
     });
-
-    // for (let i = 0; i < food.meals.length; i++) {
-    //     const element = food.meals[i];
-    //     console.log(element.strMeal);
-        
-    // }
 }
-
-// strMeal
-// strMealThumb
