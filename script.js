a699daaecce9920e1ca0c55c1159015a579ec005
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-07 01:38:59 +0600

    food aray found

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
-fetch(url)
-.then(res => res.json())
-.then(data => displayFood(data));
+const baseApi = 'https://www.themealdb.com/api/json/v1/1/search.php?f';
+
+const getFood = food => {
+    const url = `${baseApi}=${food}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayFood(data));
+}
+
+document.getElementById('search').addEventListener('click', function () {
+    const foodName = document.getElementById('food-name').value;
+    getFood(foodName);
+});
 
 const displayFood = food => {
     console.log(food);
