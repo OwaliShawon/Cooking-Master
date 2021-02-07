bed69f4b9876e2881fc65000f33bca1920a833e5
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-07 19:04:01 +0600

    details button push

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//clicking search button to call for data which go to getfood function
+document.getElementById('search').addEventListener('click', function () {
+    document.getElementById('food-items').innerHTML = " ";
+    const foodNames = document.getElementById('food-name').value;
+    getFood(foodNames);
+});
+
 // data calling function from api and format to json type
 const baseApi = 'https://www.themealdb.com/api/json/v1/1/search.php?f';
 const getFood = foodKeyWord => {
@@ -7,45 +14,30 @@ const getFood = foodKeyWord => {
         .then(data => displayFood(data));
 }
 
-//clicking search button to call for data which go to getfood function
-document.getElementById('search').addEventListener('click', function () {
-    document.getElementById('food-items').innerHTML = " ";
-    const foodNames = document.getElementById('food-name').value;
-    getFood(foodNames);
-});
-
 //display function for food name and photos which are similar to type first letter
 const displayFood = food => {
-    food.meals.forEach(singleFood => {      
-        console.log(singleFood);  
+    food.meals.forEach(singleFood => {
         const mealArea = document.createElement('div');
         const mealsDiv = document.getElementById('food-items');
         mealArea.id = 'meal-area';
         const mealInfo = `
+        <button class="btn-whole" onclick="getMeal('${singleFood.idMeal}')">
             <img class="meal-photo" src="${singleFood.strMealThumb}">
-            <h5 class="meal-name">${singleFood.strMeal}<h5>
-            <button onclick = "displayMealIngredient('${singleFood.strIngredient1} ${singleFood.strIngredient2}')">Ingredient</button>            
+            <h5 class="meal-name">${singleFood.strMeal}<h5>  
+        </button>                    
         `
         mealsDiv.appendChild(mealArea);
         mealArea.innerHTML = mealInfo;
-    });   
+    });
 }
 
-// ingredient view
-const displayMealIngredient = name => {
-    console.log(name);
+const getMeal = idMeal => {
+    const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealIngredient(data));
 }
 
-
-// food details
-// document.getElementById('meal-area').addEventListener('click', function () {
-//     console.log('clicked');
-// });
-
-// display meal ingredient
-// const displayMealIng
-
-
-// strIngredient1
-
-
+const displayMealIngredient = ingredient =>{
+    
+}
