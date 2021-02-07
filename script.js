ec9223f44ed6a3ac8b918875e8b62ec07b1be44f
Owali Ullah Shawon <owali.shawon@gmail.com>
2021-02-07 17:37:34 +0600

    atkano

--- script.js
+++ script.js
@@ -9,21 +9,43 @@ const getFood = foodKeyWord => {
 
 //clicking search button to call for data which go to getfood function
 document.getElementById('search').addEventListener('click', function () {
+    document.getElementById('food-items').innerHTML = " ";
     const foodNames = document.getElementById('food-name').value;
     getFood(foodNames);
 });
 
-//display function for food name and photos which are similar to type letter
+//display function for food name and photos which are similar to type first letter
 const displayFood = food => {
-    food.meals.forEach(singleFood => {
+    food.meals.forEach(singleFood => {      
+        console.log(singleFood);  
         const mealArea = document.createElement('div');
         const mealsDiv = document.getElementById('food-items');
-        mealArea.className = 'meal-area';
+        mealArea.id = 'meal-area';
         const mealInfo = `
             <img class="meal-photo" src="${singleFood.strMealThumb}">
-            <h5 class="meal-name">${singleFood.strMeal}<h5>            
+            <h5 class="meal-name">${singleFood.strMeal}<h5>
+            <button onclick = "displayMealIngredient('${singleFood.strIngredient1} ${singleFood.strIngredient2}')">Ingredient</button>            
         `
         mealsDiv.appendChild(mealArea);
         mealArea.innerHTML = mealInfo;
-    });
+    });   
 }
+
+// ingredient view
+const displayMealIngredient = name => {
+    console.log(name);
+}
+
+
+// food details
+// document.getElementById('meal-area').addEventListener('click', function () {
+//     console.log('clicked');
+// });
+
+// display meal ingredient
+// const displayMealIng
+
+
+// strIngredient1
+
+
